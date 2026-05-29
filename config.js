75e37a871adc8c2a86159493f35e676ddaf1efd4
anyewl <57382728+anyewl@users.noreply.github.com>
2026-05-29 17:06:13 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,30 @@
+window.APP_CONFIG = {
+    "systemTitle": "李滋芸，人工智能！",
+    "links": [
+        {
+            "id": "id_d78v2m291",
+            "title": "简介",
+            "url": "简介.html",
+            "children": []
+        },
+        {
+            "id": "id_qhkr6gtvr",
+            "title": "机器学习",
+            "url": "",
+            "children": [
+                {
+                    "id": "id_vc9mm1ud9",
+                    "title": "正则化",
+                    "url": "正则化.html",
+                    "children": []
+                },
+                {
+                    "id": "id_esrd5qjf6",
+                    "title": "KNN",
+                    "url": "KNN算法.html",
+                    "children": []
+                }
+            ]
+        }
+    ]
+};
