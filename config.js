1ae85a84ac8dfc0a447a7cc062347a48ef12fc27
anyewl <57382728+anyewl@users.noreply.github.com>
2026-05-31 19:54:39 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -13,16 +13,73 @@ window.APP_CONFIG = {
             "url": "",
             "children": [
                 {
-                    "id": "id_vc9mm1ud9",
-                    "title": "正则化",
-                    "url": "正则化.html",
+                    "id": "id_x2cp39grw",
+                    "title": "拟合问题",
+                    "url": "过拟合与欠拟合.html",
                     "children": []
+                },
+                {
+                    "id": "id_lmg3kpm87",
+                    "title": "调整参数",
+                    "url": "调参.html",
+                    "children": [
+                        {
+                            "id": "id_vc9mm1ud9",
+                            "title": "正则化",
+                            "url": "正则化.html",
+                            "children": []
+                        }
+                    ]
                 },
                 {
                     "id": "id_esrd5qjf6",
                     "title": "KNN",
                     "url": "KNN算法.html",
                     "children": []
+                },
+                {
+                    "id": "id_1mp9vmnvm",
+                    "title": "线性回归",
+                    "url": "线性回归.html",
+                    "children": [
+                        {
+                            "id": "id_7v84beed9",
+                            "title": "损失函数",
+                            "url": "损失函数.html",
+                            "children": []
+                        },
+                        {
+                            "id": "id_4vbb2nu39",
+                            "title": "梯度下降",
+                            "url": "梯度下降.html",
+                            "children": []
+                        },
+                        {
+                            "id": "id_ev8fdrnc5",
+                            "title": "梯度下降_实战案例",
+                            "url": "梯度下降_实战案例.html",
+                            "children": []
+                        },
+                        {
+                            "id": "id_m2jzeubna",
+                            "title": "模型评估",
+                            "url": "回归问题_模型评估.html",
+                            "children": []
+                        }
+                    ]
+                },
+                {
+                    "id": "id_vrchum25i",
+                    "title": "逻辑回归",
+                    "url": "",
+                    "children": [
+                        {
+                            "id": "id_cse0ssdsj",
+                            "title": "模型评估",
+                            "url": "分类问题_模型评估.html",
+                            "children": []
+                        }
+                    ]
                 }
             ]
         }
