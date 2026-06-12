cf6df96537361dd31198f69f1dfa3280e62282ca
anyewl <57382728+anyewl@users.noreply.github.com>
2026-06-12 21:05:04 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-    "systemTitle": "李滋芸，人工智能！",
+    "systemTitle": "李滋芸，人工智能！ai0321.com",
     "links": [
         {
             "id": "id_d78v2m291",
@@ -12,6 +12,61 @@ window.APP_CONFIG = {
             "title": "机器学习",
             "url": "",
             "children": [
+                {
+                    "id": "id_1qlgta02r",
+                    "title": "必要步骤",
+                    "url": "必要步骤.html",
+                    "children": [
+                        {
+                            "id": "id_5tk51iwkj",
+                            "title": "数据划分 与 交叉验证",
+                            "url": "数据划分与交叉验证.html",
+                            "children": []
+                        },
+                        {
+                            "id": "id_epewy09i2",
+                            "title": "代码+面试题",
+                            "url": "数据划分与交叉验证：代码与面试题.html",
+                            "children": []
+                        },
+                        {
+                            "id": "id_mw5tn6ss0",
+                            "title": "数据清洗：缺失值与极值",
+                            "url": "数据清洗：缺失值与极值.html",
+                            "children": []
+                        },
+                        {
+                            "id": "id_6a6fx1lh9",
+                            "title": "代码+面试题",
+                            "url": "中位数填补与 IQR 异常值截断：代码+面试题.html",
+                            "children": []
+                        },
+                        {
+                            "id": "id_200khuyl0",
+                            "title": "数据处理：归一化和标准化",
+                            "url": "归一化和标准化.html",
+                            "children": []
+                        },
+                        {
+                            "id": "id_auwnvhsgz",
+                            "title": "代码+面试",
+                            "url": "归一化与标准化：代码+面试.html",
+                            "children": []
+                        },
+                        {
+                            "id": "id_ii3vc1gnf",
+                            "title": "分类数据编码",
+                            "url": "数据编码.html",
+                            "children": []
+                        },
+                        {
+                            "id": "id_7bnfbcop3",
+                            "title": "特征构造与衍生",
+                            "url": "特征构造与衍生.html",
+                            "children": []
+                        }
+                    ]
+                },
                 {
                     "id": "id_x2cp39grw",
                     "title": "拟合问题",
@@ -28,6 +83,24 @@ window.APP_CONFIG = {
                             "title": "正则化",
                             "url": "正则化.html",
                             "children": []
+                        },
+                        {
+                            "id": "id_0bdhcwncm",
+                            "title": "归一化对正则化的影响",
+                            "url": "归一化对正则化的影响.html",
+                            "children": []
+                        },
+                        {
+                            "id": "id_ura0gdumc",
+                            "title": "网格搜索",
+                            "url": "网格搜索.html",
+                            "children": []
+                        },
+                        {
+                            "id": "id_wj4dw23g9",
+                            "title": "偏差和散度",
+                            "url": "偏差和散度.html",
+                            "children": []
                         }
                     ]
                 },
@@ -35,7 +108,14 @@ window.APP_CONFIG = {
                     "id": "id_esrd5qjf6",
                     "title": "KNN",
                     "url": "KNN算法.html",
-                    "children": []
+                    "children": [
+                        {
+                            "id": "id_1phd1vpuc",
+                            "title": "标准化的必要性",
+                            "url": "标准化的必要性.html",
+                            "children": []
+                        }
+                    ]
                 },
                 {
                     "id": "id_1mp9vmnvm",
@@ -55,15 +135,21 @@ window.APP_CONFIG = {
                             "children": []
                         },
                         {
-                            "id": "id_ev8fdrnc5",
-                            "title": "梯度下降_实战案例",
-                            "url": "梯度下降_实战案例.html",
+                            "id": "id_ek4qutxzd",
+                            "title": "梯度下降中的标准化",
+                            "url": "梯度下降中的标准化.html",
                             "children": []
                         },
                         {
                             "id": "id_m2jzeubna",
                             "title": "模型评估",
                             "url": "回归问题_模型评估.html",
+                            "children": []
+                        },
+                        {
+                            "id": "id_oabgj8xw8",
+                            "title": "代码+面试题",
+                            "url": "线性回归：代码与面试题.html",
                             "children": []
                         }
                     ]
@@ -74,9 +160,273 @@ window.APP_CONFIG = {
                     "url": "",
                     "children": [
                         {
+                            "id": "id_fxjhur70v",
+                            "title": "逻辑回归",
+                            "url": "逻辑回归.html",
+                            "children": []
+                        },
+                        {
                             "id": "id_cse0ssdsj",
                             "title": "模型评估",
                             "url": "分类问题_模型评估.html",
+                            "children": []
+                        },
+                        {
+                            "id": "id_ewyknwj4d",
+                            "title": "ROC曲线与AUC指标",
+                            "url": "ROC曲线与AUC指标.html",
+                            "children": []
+                        },
+                        {
+                            "id": "id_9yb2q566q",
+                            "title": "代码+面试题",
+                            "url": "逻辑回归：代码与面试题.html",
+                            "children": []
+                        }
+                    ]
+                },
+                {
+                    "id": "id_mbf6hintx",
+                    "title": "决策树",
+                    "url": "决策树.html",
+                    "children": [
+                        {
+                            "id": "id_hj0q7sw83",
+                            "title": "信息熵",
+                            "url": "信息熵.html",
+                            "children": []
+                        },
+                        {
+                            "id": "id_k7v7xlsdg",
+                            "title": "信息增益",
+                            "url": "信息增益.html",
+                            "children": []
+                        },
+                        {
+                            "id": "id_921y4qg8g",
+                            "title": "ID3决策树",
+                            "url": "ID3决策树.html",
+                            "children": []
+                        },
+                        {
+                            "id": "id_2b3qj1y1z",
+                            "title": "ID3的崩溃与C4.5的修正",
+                            "url": "ID3的数学崩溃与C4.5的进化修正.html",
+                            "children": []
+                        },
+                        {
+                            "id": "id_i3ets0vkq",
+                            "title": "ID3分类碎片化",
+                            "url": "CART：解决ID3分类碎片化.html",
+                            "children": []
+                        },
+                        {
+                            "id": "id_aiap9iqnf",
+                            "title": "ID3的局部最优陷阱",
+                            "url": "CART：解决ID3的局部最优陷阱.html",
+                            "children": []
+                        },
+                        {
+                            "id": "id_kzyqou8ie",
+                            "title": "CART决策树：分类问题",
+                            "url": "CART决策树.html",
+                            "children": []
+                        },
+                        {
+                            "id": "id_ozkpg6td5",
+                            "title": "CART决策树：回归问题",
+                            "url": "CART决策树：回归问题.html",
+                            "children": []
+                        },
+                        {
+                            "id": "id_1v2jltccj",
+                            "title": "集成学习",
+                            "url": "集成学习.html",
+                            "children": [
+                                {
+                                    "id": "id_699sznkxd",
+                                    "title": "随机森林",
+                                    "url": "随机森林.html",
+                                    "children": []
+                                },
+                                {
+                                    "id": "id_cpu11wz0i",
+                                    "title": "代码+面试题",
+                                    "url": "随机森林：代码与面试题.html",
+                                    "children": []
+                                },
+                                {
+                                    "id": "id_yh6e8at6m",
+                                    "title": "梯度提升",
+                                    "url": "梯度提升.html",
+                                    "children": []
+                                },
+                                {
+                                    "id": "id_0c1se7i68",
+                                    "title": "代码+面试题",
+                                    "url": "梯度提升：代码与面试题.html",
+                                    "children": []
+                                },
+                                {
+                                    "id": "id_redqwwkl3",
+                                    "title": "XGBDT的改进",
+                                    "url": "XGBDT的改进.html",
+                                    "children": []
+                                },
+                                {
+                                    "id": "id_sow8ygbjg",
+                                    "title": "代码+面试题",
+                                    "url": "XGBDT：代码+面试题.html",
+                                    "children": []
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    "id": "id_2zfm294io",
+                    "title": "K-Means聚类算法",
+                    "url": "K-Means聚类算法.html",
+                    "children": [
+                        {
+                            "id": "id_c5jdkc3zr",
+                            "title": "代码+面试题",
+                            "url": "K-Means聚类算法：代码+面试题.html",
+                            "children": []
+                        }
+                    ]
+                },
+                {
+                    "id": "id_5tls6kfjx",
+                    "title": "面试题40道",
+                    "url": "机器学习：面试题.html",
+                    "children": []
+                }
+            ]
+        },
+        {
+            "id": "id_ut96im3wg",
+            "title": "神经网络",
+            "url": "",
+            "children": [
+                {
+                    "id": "id_cds0gpvlr",
+                    "title": "简单的神经网络",
+                    "url": "nn/简单的神经网络.html",
+                    "children": []
+                },
+                {
+                    "id": "id_0a7eu535o",
+                    "title": "激活函数",
+                    "url": "nn/激活函数.html",
+                    "children": []
+                },
+                {
+                    "id": "id_cala9s2zp",
+                    "title": "参数初始化",
+                    "url": "nn/参数初始化.html",
+                    "children": []
+                },
+                {
+                    "id": "id_1hjw34yye",
+                    "title": "二分类交叉熵损失函数",
+                    "url": "nn/二分类交叉熵损失函数.html",
+                    "children": []
+                },
+                {
+                    "id": "id_xhkh66i4b",
+                    "title": "多分类交叉熵损失函数",
+                    "url": "nn/多分类交叉熵损失函数.html",
+                    "children": []
+                },
+                {
+                    "id": "id_13qik4zme",
+                    "title": "反向传播",
+                    "url": "nn/反向传播.html",
+                    "children": []
+                },
+                {
+                    "id": "id_113inzio6",
+                    "title": "backward原理：动态计算图",
+                    "url": "nn/backward原理：动态计算图.html",
+                    "children": []
+                },
+                {
+                    "id": "id_maano4u7t",
+                    "title": "数据划分 与 步长优化",
+                    "url": "nn/数据划分BatchSize.html",
+                    "children": [
+                        {
+                            "id": "id_xoj0vzjrj",
+                            "title": "核心原理：指数加权平均",
+                            "url": "nn/指数加权平均.html",
+                            "children": []
+                        },
+                        {
+                            "id": "id_y6aov1l7n",
+                            "title": "梯度优化：Momontum动量法",
+                            "url": "nn/Momontum动量.html",
+                            "children": []
+                        },
+                        {
+                            "id": "id_jkct6gnwj",
+                            "title": "学习率优化：从AdaGrad到RMSProp",
+                            "url": "nn/从AdaGrad到RMSProp.html",
+                            "children": []
+                        },
+                        {
+                            "id": "id_35j3u82ju",
+                            "title": "天下归一：Adam优化",
+                            "url": "nn/Adam算法.html",
+                            "children": []
+                        },
+                        {
+                            "id": "id_t1j9y5nof",
+                            "title": "学习率衰减策略",
+                            "url": "nn/学习率衰减策略.html",
+                            "children": []
+                        }
+                    ]
+                },
+                {
+                    "id": "id_byoy5fwis",
+                    "title": "DropOut：随机删除神经元",
+                    "url": "nn/DropOut：随机删除神经元.html",
+                    "children": []
+                },
+                {
+                    "id": "id_i5wsnuly1",
+                    "title": "批量归一化(BatchNormalization)",
+                    "url": "nn/批量归一化(BatchNormalization).html",
+                    "children": []
+                },
+                {
+                    "id": "id_anrdxgjtv",
+                    "title": "DNN手机价格分类器",
+                    "url": "nn/DNN手机价格分类器.html",
+                    "children": []
+                },
+                {
+                    "id": "id_boe70r0j7",
+                    "title": "面试题40道",
+                    "url": "深度学习：面试题.html",
+                    "children": []
+                },
+                {
+                    "id": "id_9cuufrcak",
+                    "title": "CNN",
+                    "url": "",
+                    "children": [
+                        {
+                            "id": "id_t9sgtxwy1",
+                            "title": "卷积层和池化层",
+                            "url": "nn/卷积层和池化层.html",
+                            "children": []
+                        },
+                        {
+                            "id": "id_wtqgpmp6l",
+                            "title": "CIFAR-10图片分类实战",
+                            "url": "nn/CIFAR-10图片分类实战.html",
                             "children": []
                         }
                     ]
